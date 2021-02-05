99103d07c1e9512f0ddeda3c072bd7684bcd9634
Mehmet Selcuk Bilge <m.selcukbilge@hotmail.com>
2021-02-05 17:38:02 +0000

    Update Basic_Calculator.ino

--- Examples/Basic_Calculator/Basic_Calculator.ino
+++ Examples/Basic_Calculator/Basic_Calculator.ino
@@ -28,7 +28,7 @@ void setup() {
   keypad.begin( );  
 
   lcd.begin(16, 2);
-  lcd.setCursor(0,0);lcd.print("Compact Nano Brd");
+  lcd.setCursor(0,0);lcd.print("Extended Nano ");
   delay(3000);
   lcd.setCursor(0,0);lcd.print("Welcome to MyCal");
   lcd.setCursor(0,1);   
